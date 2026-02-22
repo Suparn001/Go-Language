b51db43044f4254b9e85ae29eeaa6db8531e4345
Suparn001 <suparngautam@gmail.com>
2026-02-22 16:51:51 +0530

    arrays

---
+++ 8_arrays/arrays.go
@@ -0,0 +1,50 @@
+package main
+
+// numbered sequence of specific length
+func main() {
+	// var nums [4]int // array of 4 integers, all initialized to 0
+
+	// nums[0] = 1
+	// nums[1] = 2
+	// nums[2] = 3
+	// nums[3] = 4
+
+	// array length
+	// fmt.Println(len(nums))
+	// fmt.Println(nums)
+
+	// for i := 0; i < len(nums); i++ {
+	// 	fmt.Println(nums[i])
+	// }
+
+	// var vals [4]bool // array of 4 booleans, all initialized to false
+
+	// fmt.Println(vals)
+
+	// array of strings
+	// var names [3]string
+	// names[0] = "Alice"
+	// names[1] = "Bob"
+	// names[2] = "Charlie"
+	// fmt.Println(names)
+
+	// t0 declare it in single line
+	// nums := [3]int{1, 2, 3}
+	// fmt.Println(nums)
+
+	// declare 2d array
+	// nums := [2][3]int{{1, 2, 3}, {4, 5, 6}}
+	// // fmt.Println(nums)
+
+	// for i := 0; i < len(nums); i++ {
+	// 	for j := 0; j < len(nums[i]); j++ {
+	// 		fmt.Print(nums[i][j], " ")
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	// fixed size, that is predictble
+	// memory optimization
+	// constant time access
+
+}
